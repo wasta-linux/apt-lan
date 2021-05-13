cd3d3c817ba2a3cc333e776e496b8770b15451fd
Nate Marti <nate_marti@sil.org>
2021-05-13 13:03:09 +0100

    stop if no packages to copy

--- apt_lan/cmd.py
+++ apt_lan/cmd.py
@@ -60,6 +60,11 @@ def run_server_sync(app):
             debs_to_copy.remove(deb)
 
     logging.debug(f"Packages to copy count: {len(debs_to_copy)}")
+    if len(debs_to_copy) == 0:
+        # Already up-to-date. Nothing more to do.
+        logging.info('LAN packages already synced.\n')
+        return 0
+
     logging.debug(f"Packages to copy: {', '.join(debs_to_copy)}")
     ret = 6
 
