591d315f7c263639bf17f5d52e1c91bd837f537f
Nate Marti <nate_marti@sil.org>
2021-05-13 14:47:59 +0100

    add logging

--- apt_lan/serve-ftp.py
+++ apt_lan/serve-ftp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import logging
 import sys
 
 from pyftpdlib.authorizers import DummyAuthorizer
@@ -7,7 +8,20 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from pyftpdlib.servers import MultiprocessFTPServer
 
+
+args = sys.argv
+
+# Setup logging.
+loglevel = logging.INFO
+if 'debug' in args:
+    loglevel = logging.DEBUG
+    args.remove('debug')
+logging.basicConfig(filename='/var/log/pyftpd.log', level=loglevel)
+
+# Define share path.
 share_path = sys.argv[1]
+
+# Start server.
 authorizer = DummyAuthorizer()
 authorizer.add_anonymous(share_path, perm="elr")
 handler = FTPHandler
