eeb826ebbe01ca05ad056853e2199d00192db5a3
Nate Marti <nate_marti@sil.org>
2021-06-15 07:18:33 -0500

    add apt-lan cache as APT source

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
     data_files=[
         # Keep APT packages.
         ('/etc/apt/apt.conf.d', 'data/20keep-debs-apt-lan'),
+        # Add apt-lan cache as APT source.
+        ('/etc/apt/sources.list.d', 'data/apt-lan.list'),
         # Set base default configuration.
         ('/etc/apt-lan', [
             'data/apt-lan-rsyncd.conf',
