f68413b8cc33eed3f6223d413aa8f51114822029
Nate Marti <nate_marti@sil.org>
2021-06-15 22:32:43 -0500

    fix typo

--- apt_lan/utils.py
+++ apt_lan/utils.py
@@ -48,9 +48,9 @@ def apply_config(app):
 
         # Add link to script in dest_dir.
         if not script_path_set:
-            logging.info(f"Symlinking /usr/lib/{app.name}/{v} into {dest_dir}.")
+            logging.info(f"Symlinking /usr/lib/{app.pkg_name}/{v} into {dest_dir}.")
             dest_path = dest_dir / v
-            dest_path.symlink_to(f"/usr/lib/{app.name}/{v}")
+            dest_path.symlink_to(f"/usr/lib/{app.pkg_name}/{v}")
     logging.info("Config applied.")
 
 def find_script(script_name):
